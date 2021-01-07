ca77fb23a825816c8408080e7b1072d138d5a11d
Basil Fierz <basil.fierz@hotmail.com>
2021-01-07 14:53:39 +0100

    Remove outdated multi-build option

--- recipes/vcl/all/conanfile.py
+++ recipes/vcl/all/conanfile.py
@@ -19,14 +19,12 @@ class LibnameConan(ConanFile):
 
     settings = "os", "arch", "compiler", "build_type"
     options = {
-            "multi_build": [True, False],
             "vectorization": ["AVX", "AVX 2", "SSE 4.2" ], 
             "d3d12": [True, False],
             "opengl": [True, False],
             "fPIC": [True, False]
             }
     default_options = {
-            "multi_build": False,
             "vectorization": "AVX",
             "d3d12": False,
             "opengl": False,
@@ -50,12 +48,6 @@ class LibnameConan(ConanFile):
         return "build_subfolder"
 
     def config_options(self):
-
-        # Support multi-package configuration
-        if self.options.multi_build:
-            del self.settings.build_type
-            if self.settings.compiler == "Visual Studio":
-                del self.settings.compiler["Visual Studio"].runtime
 
         if self.settings.os == 'Windows':
             del self.options.fPIC
@@ -86,10 +78,6 @@ class LibnameConan(ConanFile):
         cmake.definitions["VCL_BUILD_TOOLS"] = False
         cmake.definitions["VCL_BUILD_EXAMPLES"] = False
 
-        # Support multi-package configuration
-        if not hasattr(self.settings, "build_type"):
-            cmake.definitions["CMAKE_DEBUG_POSTFIX"] = "_d"
-
         # Configure features
         cmake.definitions["VCL_CXX_STANDARD"] = self.settings.compiler.cppstd if self.settings.compiler.cppstd else 14
         cmake.definitions["VCL_VECTORIZE"] = str(self.options.vectorization)
@@ -113,28 +101,10 @@ class LibnameConan(ConanFile):
         cmake = self._configure_cmake()
         cmake.configure(build_folder=self._build_subfolder)
 
-        if not hasattr(self.settings, "build_type"):
-            if cmake.is_multi_configuration:
-                cmake.build(target="vcl_core",     args=["--config","Debug"])
-                cmake.build(target="vcl.geometry", args=["--config","Debug"])
-                cmake.build(target="vcl_graphics", args=["--config","Debug"])
-                cmake.build(target="vcl.math",     args=["--config","Debug"])
-                cmake.build(target="vcl_core",     args=["--config","Release"])
-                cmake.build(target="vcl.geometry", args=["--config","Release"])
-                cmake.build(target="vcl_graphics", args=["--config","Release"])
-                cmake.build(target="vcl.math",     args=["--config","Release"])
-            else:
-                for config in ("Debug", "Release"):
-                    self.output.info("Building %s" % config)
-                    cmake.definitions["CMAKE_BUILD_TYPE"] = config
-                    cmake.configure(build_folder=self._build_subfolder)
-                    shutil.rmtree("CMakeFiles")
-                    os.remove("CMakeCache.txt")
-        else:
-            cmake.build(target="vcl_core")
-            cmake.build(target="vcl.geometry")
-            cmake.build(target="vcl_graphics")
-            cmake.build(target="vcl.math")
+        cmake.build(target="vcl_core")
+        cmake.build(target="vcl.geometry")
+        cmake.build(target="vcl_graphics")
+        cmake.build(target="vcl.math")
 
         cmake.install()
     def package(self):
@@ -160,33 +130,7 @@ class LibnameConan(ConanFile):
 
         self.cpp_info.includedirs = ['include/vcl_core', 'include/vcl.geometry', 'include/vcl_graphics', 'include/vcl.math']
         if self.settings.os == "Windows":
-            # ideally it should be possible to have avx compilation flags forwarded from the recipe
-            # but the current management of nvcc integration doesn't play well with cflags and cxxflags
-            # avx_flag = ""
-            # if self.options.vectorization == "AVX":
-            #     avx_flag = "/arch:AVX"
-            # if self.options.vectorization == "AVX 2":
-            #     avx_flag = "/arch:AVX2"
-            # self.cpp_info.cflags = [avx_flag]
-            # self.cpp_info.cxxflags = [avx_flag]
-            
-            if not hasattr(self.settings, "build_type"):
-                self.cpp_info.debug.libs = ['vcl_core_d.lib', 'vcl.math_d.lib', 'vcl.geometry_d.lib', 'vcl_graphics_d.lib']
-                self.cpp_info.release.libs = ['vcl_core.lib', 'vcl.math.lib', 'vcl.geometry.lib', 'vcl_graphics.lib']
-            else:
-                self.cpp_info.libs = ['vcl_core.lib', 'vcl.math.lib', 'vcl.geometry.lib', 'vcl_graphics.lib']
+            self.cpp_info.libs = ['vcl_core.lib', 'vcl.math.lib', 'vcl.geometry.lib', 'vcl_graphics.lib']
         else:
-            # ideally it should be possible to have avx compilation flags forwarded from the recipe
-            # but the current management of nvcc integration doesn't play well with cflags and cxxflags
-            # avx_flag = ""
-            # if self.options.vectorization == "AVX":
-            #     avx_flag = "-mavx"
-            # if self.options.vectorization == "AVX 2":
-            #     avx_flag = "-mavx2"
-            # if self.options.vectorization == "SSE 4.2":
-            #     avx_flag = "-msse4.2"
-            # self.cpp_info.cflags = [avx_flag]
-            # self.cpp_info.cxxflags = [avx_flag]
-
             self.cpp_info.libs = ['libvcl_core.a', 'libvcl.math.a', 'libvcl.geometry.a', 'libvcl_graphics.a']
         self.cpp_info.libdirs = [ "lib" ]
